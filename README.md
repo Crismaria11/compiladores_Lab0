f9f231bddddb359a4e7df648e98a98eae83cf928
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2022-07-15 22:45:40 -0600

    ADD: Video test & files
    UPDATE: Readme and Test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # compiladores_Lab0
+
+antlr4 -Dlanguage=Python3 prueba.g4 -visitor
+
+pygrun prueba program --tree test.cl
+
+https://user-images.githubusercontent.com/59905458/179339694-3227a811-3bc7-4513-8646-13a46186f574.mp4
+
--- main.py
+++ main.py
@@ -4,45 +4,12 @@ from dist.pruebaLexer import pruebaLexer
 from dist.pruebaParser import pruebaParser
 from dist.pruebaVisitor import pruebaVisitor
 
-
-def get_username():
-    from pwd import getpwuid
-    from os import getuid
-    return getpwuid(getuid())[ 0 ]
-
-
-class visitor(MyGrammerVisitor):
-    def visitNumberExpr(self, ctx):
-        value = ctx.getText()
-        return int(value)
-
-    def visitParenExpr(self, ctx):
-        return self.visit(ctx.expr())
-
-    def visitInfixExpr(self, ctx):
-        l = self.visit(ctx.left)
-        r = self.visit(ctx.right)
-
-        op = ctx.op.text
-        operation =  {
-        '+': lambda: l + r,
-        '-': lambda: l - r,
-        '*': lambda: l * r,
-        '/': lambda: l / r,
-        }
-        return operation.get(op, lambda: None)()
-
-    def visitByeExpr(self, ctx):
-        print(f"goodbye {get_username()}")
-        sys.exit(0)
-
-    def visitHelloExpr(self, ctx):
-        return (f"{ctx.getText()} {get_username()}")
-
-
 if __name__ == "__main__":
-    while 1:
-        data =  InputStream(input(">>> "))
+    if len(sys.argv) > 1:
+        data = FileStream(sys.argv[1])
+    else:
+        data = InputStream(sys.stdin.readline())
+
         # lexer
         lexer = pruebaLexer(data)
         stream = CommonTokenStream(lexer)
@@ -50,6 +17,8 @@ if __name__ == "__main__":
         parser = pruebaParser(stream)
         tree = parser.expr()
         # evaluator
-        visitor = visitor()
+        visitor = pruebaVisitor()
         output = visitor.visit(tree)
+        tree_string = tree.toStringTree(recog=parser)
+        print(tree_string)
         print(output)
